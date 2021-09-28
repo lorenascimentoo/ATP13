51f0dbaa065ee545c24adf110d713cfad4454e17
lorenascimentoo <lorenanascimento28@gmail.com>
2021-09-28 16:59:23 -0300

    implementacao do metodo de escolha do menu e encaminha pra opcao desejada

--- src/modulo1/atp14.java
+++ src/modulo1/atp14.java
@@ -8,18 +8,22 @@ public class atp14 {
 
         exibirMenu();
         int opcao = lerOpcao("Digite uma opção para cálculo: ");
+
+        selecionarMenu(opcao);
     }
+
     static void cabecalho(){
         System.out.println("********Calculadora********");
     }
 
     static void exibirMenu(){
         System.out.println("** Opções da calculadora **");
-        System.out.println("1 - Somar");
-        System.out.println("2 - Subtrair");
-        System.out.println("3 - Multiplicar");
-        System.out.println("4 - Dividir");
+        System.out.println("1 - Soma");
+        System.out.println("2 - Subtração");
+        System.out.println("3 - Multiplicação");
+        System.out.println("4 - Divisão");
     }
+
     static int lerOpcao(String mensagem){
         Scanner scan = new Scanner(System.in);
         int numero;
@@ -36,5 +40,42 @@ public class atp14 {
             validacao = false;
         } 
         return validacao;
+    }
+
+    static void selecionarMenu(int op){
+        switch (op) {
+            case 1:
+                System.out.println("==== SOMA ===");
+                somar();
+                break;
+            case 2:
+                System.out.println("==== SUBTRAÇÃO ===");
+                subtrair();
+                break;
+            case 3:
+                System.out.println("==== MULTIPLICAÇÃO ===");
+                multiplicar();
+                break;
+            case 4:
+                System.out.println("==== DIVISÃO ===");
+                dividir();
+                break;
+        }
+    }
+
+    static void somar(){
+
+    }
+
+    static void subtrair(){
+        
+    }
+
+    static void multiplicar(){
+        
+    }
+
+    static void dividir(){
+        
     }
 }
