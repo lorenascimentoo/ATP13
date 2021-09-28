a6730e9c242aa3859530afd7eb608bf74f8d990e
lorenascimentoo <lorenanascimento28@gmail.com>
2021-09-28 16:28:45 -0300

    implementacao da exibicao do menu

--- src/modulo1/atp14.java
+++ src/modulo1/atp14.java
@@ -3,8 +3,18 @@ package modulo1;
 public class atp14 {
     public static void main(String[]args){
         cabecalho();
+
+        exibeMenu();
     }
     static void cabecalho(){
         System.out.println("********Calculadora********");
+    }
+
+    static void exibeMenu(){
+        System.out.println("** Opções da calculadora **");
+        System.out.println("1 - Somar");
+        System.out.println("2 - Subtrair");
+        System.out.println("3 - Multiplicar");
+        System.out.println("4 - Dividir");
     }
 }
