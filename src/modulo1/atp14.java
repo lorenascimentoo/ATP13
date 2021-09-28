4eaecaf842075c5d0274ef4eeb4d6cae79c24f73
Raissa M. Lima <raissamartinsarq@gmail.com>
2021-09-28 16:48:21 -0300

    Implementação de leitura para opção

--- src/modulo1/atp14.java
+++ src/modulo1/atp14.java
@@ -1,20 +1,40 @@
 package modulo1;
 
+import java.util.Scanner;
+
 public class atp14 {
     public static void main(String[]args){
         cabecalho();
 
-        exibeMenu();
+        exibirMenu();
+        int opcao = lerOpcao("Digite uma opção para cálculo: ");
     }
     static void cabecalho(){
         System.out.println("********Calculadora********");
     }
 
-    static void exibeMenu(){
+    static void exibirMenu(){
         System.out.println("** Opções da calculadora **");
         System.out.println("1 - Somar");
         System.out.println("2 - Subtrair");
         System.out.println("3 - Multiplicar");
         System.out.println("4 - Dividir");
+    }
+    static int lerOpcao(String mensagem){
+        Scanner scan = new Scanner(System.in);
+        int numero;
+        do{
+            System.out.print(mensagem);
+            numero = Integer.parseInt(scan.nextLine());
+        }while (!validarOpcao(numero));
+        return numero;
+    }
+    static boolean validarOpcao(int numero){
+        boolean validacao = true;
+        if (numero <= 0 || numero > 4){
+            System.out.println("Opção Inválida");
+            validacao = false;
+        } 
+        return validacao;
     }
 }
