f2ac58a62baa6621adb5bd3ec7d85435b1a1d73c
Raissa M. Lima <raissamartinsarq@gmail.com>
2021-09-28 10:13:43 -0300

    Implementação do Método de Soma

--- src/modulo1/Calculadora.java
+++ src/modulo1/Calculadora.java
@@ -4,9 +4,15 @@ public class Calculadora {
 
     public static void main(String[] args) {
         cabecalho();
+        soma(10, 20);
     }
 
     static void cabecalho(){
         System.out.println("========= CALCULO DE MEDIA =========");
+    }
+    static int soma(int n1, int n2){
+        int resultado = n1 + n2; 
+        System.out.print(resultado);
+        return resultado; 
     }
 }
