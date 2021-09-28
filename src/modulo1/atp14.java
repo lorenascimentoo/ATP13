d1256ac6482355d8730e0f17e07f119f46de2bb5
Raissa M. Lima <raissamartinsarq@gmail.com>
2021-09-28 16:22:38 -0300

    Implementando Cabeçalho

---
+++ src/modulo1/atp14.java
@@ -0,0 +1,10 @@
+package modulo1;
+
+public class atp14 {
+    public static void main(String[]args){
+        cabecalho();
+    }
+    static void cabecalho(){
+        System.out.println("********Calculadora********");
+    }
+}
