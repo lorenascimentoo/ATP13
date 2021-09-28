41744fa782b136d024ac5bcb5d05ddd11489a664
lorenascimentoo <lorenanascimento28@gmail.com>
2021-09-28 09:58:35 -0300

    implementacao do cabecalho

---
+++ src/modulo1/Calculadora.java
@@ -0,0 +1,12 @@
+package modulo1;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        cabecalho();
+    }
+
+    static void cabecalho(){
+        System.out.println("========= CALCULO DE MEDIA =========");
+    }
+}
