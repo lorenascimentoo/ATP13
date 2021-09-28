ba5704d87f8d1d6bb1e7ac2806ebdae56bf39bb6
lorenascimentoo <lorenanascimento28@gmail.com>
2021-09-28 10:21:14 -0300

    implementacao do metodo de calculo de media

--- src/modulo1/Calculadora.java
+++ src/modulo1/Calculadora.java
@@ -5,14 +5,22 @@ public class Calculadora {
     public static void main(String[] args) {
         cabecalho();
         soma(10, 20);
+        media(10,8, 7, 10);
     }
 
     static void cabecalho(){
         System.out.println("========= CALCULO DE MEDIA =========");
     }
+
     static int soma(int n1, int n2){
         int resultado = n1 + n2; 
-        System.out.print(resultado);
+        System.out.println(resultado);
         return resultado; 
+    }
+
+    static double media(int n1, int n2, int n3, int n4){
+        double resultMedia = (n1+n2+n3+n4)/4;
+        System.out.println(resultMedia);
+        return resultMedia;
     }
 }
